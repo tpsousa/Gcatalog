2a176a2db5581978453af62d28867be884831fd7
TLOQX <tpsousa.eng@gmail.com>
2026-07-25 19:30:33 -0300

    h2 database

--- back-end/src/main/java/com/work/dscatalog/services/CategoryService.java
+++ back-end/src/main/java/com/work/dscatalog/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.work.dscatalog.services;
 
 import com.work.dscatalog.entities.Category;
 import com.work.dscatalog.repositories.CategoryRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,7 @@ public class CategoryService {
     @Autowired
     private CategoryRepository repository;
 
+    //@Transactional(readOnly = true)
     public List<Category> findAll(){
 
         return repository.findAll();
