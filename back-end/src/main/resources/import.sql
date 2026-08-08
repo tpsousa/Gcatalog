647cab9ec61d6d1c02be76b5305de9fffa728559
TLOQX <tpsousa.eng@gmail.com>
2026-08-08 20:23:06 -0300

    fix : deleted the basics tests

--- back-end/src/main/resources/import.sql
+++ back-end/src/main/resources/import.sql
@@ -10,7 +10,7 @@ INSERT INTO tb_category (name, created_at) VALUES ('perola', NOW());
 
 INSERT INTO tb_category (name, created_at) VALUES ('ratinha', NOW());
 
-INSERT INTO tb_category (name, created_at) VALUES ('jarvis', NOW());
+INSERT INTO tb_category (name, created_at) VALUES ('jarvis', NO W());
 
 INSERT INTO tb_category (name, created_at) VALUES ('coca-cola', NOW());
 
